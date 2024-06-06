173efeb900ef1eba0d04d9efbb9d69c7f16d3ac6
fazerlazer2101 <samferrer145@gmail.com>
2024-06-06 17:12:39 -0500

    Added config files
    Added configuration files to store user settings.

--- main.py
+++ main.py
@@ -4,16 +4,17 @@ import os
 import tkinter
 from tkinter import filedialog
 
-
+import yaml
 
 #Menu
 menu_options = {
     1: 'Connect',
-    2: 'Change Source',
-    3: 'Change Server',
-    4: 'Change Credentials',
-    5: 'Exit'
+    2: 'Change Server',
+    3: 'Change Credentials',
+    4: 'Exit'
 }
+
+
 
 #ftp menu
 ftp_options = {
@@ -28,12 +29,13 @@ ftp_options = {
 #ftp instance
 ftp = FTP()
 
+# Method handles ftp menu responses.
 def ftp_response_menu():
     for key in ftp_options.keys():
         print (key, '--', ftp_options[key] )
     validation()
 
-
+# Method that Handles user input validation
 valid_response = 5    
 def validation():
     valid = True
@@ -47,25 +49,84 @@ def validation():
         except Exception:
             validation()
 
-#Prints options for the ftp client
+# Method that prints options for the ftp client
 def user_response_menu():
     
     print ("Welcome to the FTP transfer Tool Please select an option.")
     for key in menu_options.keys():
         print (key, '--', menu_options[key] )
+        continue
     validation()
- 
+    #Manage user option
+    if valid_response == 1:
+        print("Start Transfer")
+        try:
+            #Read config
+            with open('serverConfig.yaml', 'r') as server:
+                data = yaml.safe_load(server)
+                
+            with open('userConfig.yaml', 'r') as user:
+                user_data = yaml.safe_load(user)
+            #Starts connection
+            ftp.connect(host=f"{data["host"]}",port=int(data["port"]))
+            ftp.login(user=f"{user_data["user"]}", passwd=f"{user_data["password"]}")
+            
+        except Exception:
+            print("Failed to connect")
+            user_response_menu()
+        ftp_control()
+    elif valid_response == 2:
+        change_server_settings()
+        
+    elif valid_response == 3:
+        change_user_settings()
+    else:
+        exit()
+        
 
-
+#Method to Create a directory
 def create_directory():
     folder_name = input("Name of Directory: ")
     ftp.mkd(folder_name)
 
-
+#Method to change directory in
 def change_directory():
     path = input("Path: ")
     ftp.cwd(path)
-#Checks if an item is a directory
+    
+# Method to change the connection / server settings.
+def change_server_settings():
+    new_host = input("Enter new server: ")
+    #Validates port to be only a number.
+    try:
+        new_port = int(input("Enter new port: "))
+    except Exception as e:
+        change_user_settings()      
+    data = {
+        "host": f"{str(new_host)}",
+        "port": int(new_port),
+    }
+    #Dumps into a yaml file
+    with open('serverConfig.yaml', 'w') as file:
+        yaml.dump(data, file)
+    user_response_menu()
+    
+# Method that changes user configuration
+def change_user_settings():
+    new_username = input("Enter username: ")
+    #Validates port to be only a number.
+    new_password = input("Enter your password: ")
+    data_user = {
+        "user": f"{str(new_username)}",
+        "password": f"{new_password}",
+    }
+    #Dumps into a yaml file
+    with open('userConfig.yaml', 'w') as userfile:
+        yaml.dump(data_user, userfile)
+        
+    user_response_menu()
+
+# Method that checks if an item is a directory
 def check_if_directory(path, item):
   item_path = os.path.join(path, item)
   if os.path.isfile(item_path):
@@ -73,7 +134,7 @@ def check_if_directory(path, item):
   else:
       return False
     
-    
+# Method that handles uploading folders
 def upload_folder(local_path, remote_path):
     #Parent folder name
     local_directory_name = os.path.basename(local_path)
@@ -92,8 +153,7 @@ def upload_folder(local_path, remote_path):
     #List of files in local directory
     local_files = os.listdir(local_path)
     
-    
-    #Iterate over the local directory
+    #Iterate over the local directory.
     for item in local_files:
         #Determine if item is a file or folder
         if check_if_directory(local_path, item):
@@ -119,8 +179,10 @@ def upload_folder(local_path, remote_path):
             print(f"New path: {os.path.join(local_path, item)}")
             #Go into that directory
             upload_folder(os.path.join(local_path, item), os.path.join(local_path, item))
-    ftp.cwd("..")   
+    ftp.cwd("..")
+    ftp_control()   
 
+# Method that handles individual file upload.
 def upload_files():
     tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
 
@@ -161,7 +223,7 @@ def upload_files():
         except Exception as e:
             print(e)
             
-
+# Method that handles the ftp control responses.
 def ftp_control():
     if True:
             #FTP menu
@@ -193,28 +255,5 @@ def ftp_control():
             else:
                 exit()
 
-
 #Start of application
 user_response_menu()
-
-
-#Manage user option
-if valid_response == 1:
-    print("Start Transfer")
-    try:
-        #Starts connection
-        ftp.connect(host="10.0.0.16",port=2121)
-        ftp.login(user="deck", passwd="1506")
-        
-    except Exception:
-        print("Failed to connect")
-        user_response_menu()
-    ftp_control()
-elif valid_response == 2:
-    print("Change Source")
-elif valid_response == 3:
-    print("Change Server")
-elif valid_response == 4:
-    print("Change Credentials")
-else:
-    exit()

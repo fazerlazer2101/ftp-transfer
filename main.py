fe424bd854f6d4f8dea890e0e393ddb73b319ea2
fazerlazer2101 <samferrer145@gmail.com>
2024-06-06 16:01:18 -0500

    Recursive functions
    Working recursive file transfer.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from ftplib import FTP
+import os
+
+import tkinter
+from tkinter import filedialog
+
+
 
 #Menu
 menu_options = {
@@ -15,7 +21,8 @@ ftp_options = {
     2: 'Create Directory',
     3: 'List Current Directory',
     4: 'Change Directory',
-    5: 'Exit'
+    5: 'Current Directory',
+    6: 'Exit'
 }
 
 #ftp instance
@@ -40,7 +47,7 @@ def validation():
         except Exception:
             validation()
 
-#Prints a menu
+#Prints options for the ftp client
 def user_response_menu():
     
     print ("Welcome to the FTP transfer Tool Please select an option.")
@@ -48,50 +55,161 @@ def user_response_menu():
         print (key, '--', menu_options[key] )
     validation()
  
-user_response_menu()
+
 
 def create_directory():
-    folder_name = input("Name of Directory:")
+    folder_name = input("Name of Directory: ")
     ftp.mkd(folder_name)
 
 
-control = True
-def ftp_control():
+def change_directory():
+    path = input("Path: ")
+    ftp.cwd(path)
+#Checks if an item is a directory
+def check_if_directory(path, item):
+  item_path = os.path.join(path, item)
+  if os.path.isfile(item_path):
+      return True
+  else:
+      return False
     
+    
+def upload_folder(local_path, remote_path):
+    #Parent folder name
+    local_directory_name = os.path.basename(local_path)
+    #Change to the remote directory
+    print(remote_path)
+    
+    #Check if parent folder exists in remote
+    try:
+        ftp.cwd(local_directory_name)
+    except Exception as e:
+        print("No Folder found")
+        ftp.mkd(local_directory_name)
+        #Goes to parent folder
+        ftp.cwd(local_directory_name)
+    
+    #List of files in local directory
+    local_files = os.listdir(local_path)
+    
+    
+    #Iterate over the local directory
+    for item in local_files:
+        #Determine if item is a file or folder
+        if check_if_directory(local_path, item):
+            #Copy file
+            print(f"Copied to {ftp.pwd()} {item}")
+            try:
+                file_name = str(item)
+                folder_path = os.path.join(local_path, item)
+                with open(folder_path, 'rb') as file:
+                    ftp.storbinary(f"STOR {file_name}",file, 8192)
+                    print("Transfer Complete")
+            except Exception as e:
+                print(e)
+        else:
+            #Make Directory
+            print(f"{item}")
+            #Fails means that directory exists
+            try:
+                ftp.mkd(f"{item}")
+            except Exception as e:
+                pass
+            local_files = os.listdir(os.path.join(local_path, item))
+            print(f"New path: {os.path.join(local_path, item)}")
+            #Go into that directory
+            upload_folder(os.path.join(local_path, item), os.path.join(local_path, item))
+    ftp.cwd("..")   
+
+def upload_files():
+    tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
+
+    
+    folder_path = ""
+    
+    #Ask if you are transfering a file or folder
+    file_options = {
+        1: "Transfer a file?",
+        2: "Transfer a folder?",
+        3: "Exit"
+    }
+    
+    for key in file_options.keys():
+        print (key, '--', file_options[key] )
+    validation()
+    
+    
+    
+    if valid_response == 1:
+        folder_path = filedialog.askopenfilename()
+    elif valid_response == 2:
+        folder_path = filedialog.askdirectory()
+    else:
+        ftp_control()
+    
+    #Recursivly check start transfer of a folder
+    
+    #First check if path recieved is a folder or a file
+    if os.path.isdir(str(folder_path)):
+        upload_folder(folder_path, ftp.pwd())
+    else:
+        try:
+            file_name = os.path.basename(str(folder_path))
+            with open(folder_path, 'rb') as file:
+                ftp.storbinary(f"STOR {file_name}",file, 8192)
+                print("Transfer Complete")
+        except Exception as e:
+            print(e)
+            
+
+def ftp_control():
     if True:
             #FTP menu
             ftp_response_menu()
 
             #respond to inputs
             if valid_response == 1:
-                print("Transfer Files")
-                ftp_control()
+                print("Transfer files to the directory " +ftp.pwd())
+                confirmation = input("Continue? Y/n ")
+                if(confirmation.__eq__("Y")):
+                    upload_files()
+                else:
+                    ftp_control()
             elif valid_response == 2:
                 print("Create Directory")
                 create_directory()
                 ftp_control()
             elif valid_response == 3:
-                print("List Current Directory")
-                print(ftp.pwd())
+                currentfiles = ftp.nlst()
+                for item in currentfiles:
+                    print(item)
                 ftp_control()
             elif valid_response == 4:
-                print("Change Directory")
+                change_directory()
+                ftp_control()
+            elif valid_response== 5:
+                print(ftp.pwd())
                 ftp_control()
             else:
-               global control
-               control = False
+                exit()
 
+
+#Start of application
+user_response_menu()
 
 
 #Manage user option
 if valid_response == 1:
     print("Start Transfer")
-    ftp.connect(host="10.0.0.16",port=2121)
-    ftp.login(user="deck", passwd="1506")
-    #Starts connection
-    #Connect to FTP server
+    try:
+        #Starts connection
+        ftp.connect(host="10.0.0.16",port=2121)
+        ftp.login(user="deck", passwd="1506")
+        
+    except Exception:
+        print("Failed to connect")
+        user_response_menu()
     ftp_control()
-    
 elif valid_response == 2:
     print("Change Source")
 elif valid_response == 3:
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,1 @@
-ftp_options = {
-#     1: 'Transfer Files',
-#     2: 'Create Directory',
-#     3: 'List Current Directory',
-#     4: 'Change Directory',
-#     5: 'Exit'
-# }
+lder_path
